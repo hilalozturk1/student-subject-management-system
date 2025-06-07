a53c7bde09045cee127db1bb7eceae338aab8850
hilalozturk1 <hlloztrk68@gmail.com>
2025-06-07 22:52:34 +0300

    students - enrollments - courses pages

---
+++ frontend/src/app/dashboard/enrollments/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Enrollments() {
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm">
+        <h2 className="text-2xl font-bold text-center mb-6">Enrollments</h2>
+      </div>
+    </div>
+  );
+}
